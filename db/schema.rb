4a6e2ccfea3b9d1eda4857acd3a2a7545a8754e9
yuto.eguma <yuto.eguma@gmail.com>
2023-01-08 14:59:19 +0900

    unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_144517) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_055244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
