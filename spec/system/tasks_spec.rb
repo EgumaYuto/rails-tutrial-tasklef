b24fb088574ac9fa7d81e9500f51c5f7b1bf2b94
yuto.eguma <yuto.eguma@gmail.com>
2023-01-14 13:59:31 +0900

    use shared examples

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,13 +12,15 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザAが作成したタスクが表示される' do
+    it { expect(page).to have_content task_a.name }
+  end
+
   describe '一覧表示機能' do
     context 'ユーザAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザAが作成したタスクが表示される' do
-        expect(page).to have_content task_a.name
-      end
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
     end
 
     context 'ユーザBがログインしているとき' do
@@ -38,9 +40,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content task_a.name
-      end
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
     end
   end
 end
